22c2694e3c537a1be80b119ac10d08f2990e954d
nevaliashka-no-hacker <nevaliashka2007@mail.ru>
2026-05-19 17:35:17 +0300

    добавление pre-commit)

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Репозиторий для хранения моих работ по предмету 'Промышленное программирование'
+
+Инструкция для pre-commit hook:
+cp pre-commit.sh .git/hooks/pre-commit
+chmod +x .git/hooks/pre-commit
---
+++ pre-commit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Running pre-commit hook..."
+
+echo "Checking code style with clang-format..."
+echo "clang-format -i *.cpp *.hpp"
+
+echo "Checking code style with clang-tidy"
+echo "clang-tidy --fix *.cpp *.hpp -- -std=c++17"
+
+echo "Checking code style with cppcheck";
+echo "cppcheck --enable=all"
