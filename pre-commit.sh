873a1cb43a7cc2564a5ef45b3cdc69d7bb0730ae
nevaliashka-no-hacker <nevaliashka2007@mail.ru>
2026-05-19 17:37:22 +0300

    подправила в pre-commit, чтобы красивенько все было

--- pre-commit.sh
+++ pre-commit.sh
@@ -5,8 +5,8 @@ echo "Running pre-commit hook..."
 echo "Checking code style with clang-format..."
 echo "clang-format -i *.cpp *.hpp"
 
-echo "Checking code style with clang-tidy"
+echo "Checking code style with clang-tidy..."
 echo "clang-tidy --fix *.cpp *.hpp -- -std=c++17"
 
-echo "Checking code style with cppcheck";
+echo "Checking code style with cppcheck...";
 echo "cppcheck --enable=all"
