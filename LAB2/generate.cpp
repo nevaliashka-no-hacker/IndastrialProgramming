afe60ae72e3d4721118029005f2c797cd269435c
nevaliashka-no-hacker <nevaliashka2007@mail.ru>
2026-05-19 13:34:56 +0300

    произошла небольшая путаница у первого, исправлено

--- LAB2/generate.cpp
+++ LAB2/generate.cpp
@@ -1,33 +1,39 @@
 #include "generate.hpp"
 
-void generate(std::string data) {
+void generate(std::string data, std::string filename) {
+
 	try {
+		// режим шбтрихкода 128
 		ZXing::MultiFormatWriter writer(ZXing::BarcodeFormat::Code128);
 
+		// принимает на любом языке (русский и английский), пространство вокруг штрихкода 
 		writer.setEncoding(ZXing::CharacterSet::UTF8);
 		writer.setMargin(20);
 
-		//std::string data = "1234567890";
-
+		// матрица из 0 и 1 - неотформатированный штрихкод
 		auto matrix = writer.encode(data, 300, 100);
 
+		// будем увеличивать изображение в SCALE раз
 		const int SCALE = 20;
 
 		int imgWidth = matrix.width() * SCALE;
-        	int imgHeight = matrix.height() * SCALE;
-        	cv::Mat barcodeImage(imgHeight, imgWidth, CV_8UC3, cv::Scalar(255, 255, 255)); // Белый фон
+        int imgHeight = matrix.height() * SCALE;
+		// белый фон
+        cv::Mat barcodeImage(imgHeight, imgWidth, CV_8UC3, cv::Scalar(255, 255, 255)); 
         
-        	for (int y = 0; y < matrix.height(); ++y) {
-                	for (int x = 0; x < matrix.width(); ++x) {
-                    		if (matrix.get(x, y)) {
+		// рисуем прямоугольнички и закрашиваем, так получается штрихкод в 
+		// понятном нам виде
+        for (int y = 0; y < matrix.height(); ++y) {
+            for (int x = 0; x < matrix.width(); ++x) {
+                if (matrix.get(x, y)) {
 					cv::rectangle(barcodeImage,
-							cv::Point(x * SCALE, y * SCALE),
-							cv::Point((x + 1) * SCALE, (y + 1) * SCALE),
-							cv::Scalar(0, 0, 0),
-							cv::FILLED);
-                		}
-            		}
-        	}
+					cv::Point(x * SCALE, y * SCALE),
+					cv::Point((x + 1) * SCALE, (y + 1) * SCALE),
+					cv::Scalar(0, 0, 0),
+					cv::FILLED);
+                }
+            }
+        }
 		
 		int mode;
 		std::cout << "1 or 2? ";
@@ -35,18 +41,19 @@ void generate(std::string data) {
 
 		if (mode)
 		{
-			std::string outputFilename = "barcode.png";
-			cv::imwrite(outputFilename, barcodeImage);
+			// сохраняем получившееся изображение в файлик
+			cv::imwrite(filename + ".png", barcodeImage);
 			std::cout << "The barcode has been successfully created in file " << outputFilename << std::endl;
 		}
 		else
 		{
+			// показать изображение в окне
 			cv::imshow("Сгенерированный штрихкод", barcodeImage);
 			cv::waitKey(0);
 		}
-		
 
 	} catch (const std::exception& e) {
+		// ловим ошибку и обрабатываем
 		std::cerr << "Ошибка: " << e.what() << std::endl;
 	}
 }
